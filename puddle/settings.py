57e54029f33d487712c6f62c5de9fd6e9c1158b4
EC2 Default User <ec2-user@ip-172-31-45-81.eu-west-1.compute.internal>
2024-04-24 23:26:23 +0000

    last

--- puddle/settings.py
+++ puddle/settings.py
@@ -77,14 +77,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'puddle.wsgi.application'
 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # Correct way to define path with os.path.join
     }
 }
-
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
